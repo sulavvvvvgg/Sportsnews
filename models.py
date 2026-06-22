c8ab054680eee0092419ed5f5a006b821486f461
Sulav Gurung <sulavgrg09@email.com>
2026-06-22 11:31:22 +0545

    Connection to database and created article model

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, default=db.func.now())
+
+    def __repr__(self):
+        return f"<Article {self.id} - {self.title}>"
