7d91b4eb354d390364fd2c36af283704e0536dc8
Sulav Gurung <sulavgrg09@email.com>
2026-06-25 13:55:33 +0545

    added edit and delete routes for articles

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from models import db, Article
 
 app = Flask(__name__)
@@ -9,7 +9,46 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    articles = Article.query.all()
+    return render_template('index.html', articles=articles)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        category = request.form['category']
+        author = request.form['author']
+
+        new_article = Article(title=title, content=content, category=category, author=author)
+        db.session.add(new_article)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template('create_article.html')
+
+@app.route('/edit/<int:article_id>', methods=['GET', 'POST'])
+def edit_article(article_id):
+    article = Article.query.get(article_id)
+
+    if request.method == 'POST':
+        article.title = request.form['title']
+        article.content = request.form['content']
+        article.category = request.form['category']
+        article.author = request.form['author']
+
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template('edit_article.html', article=article)
+
+@app.route('/delete/<int:article_id>')
+def delete_article(article_id):
+    article = Article.query.get(article_id)
+    db.session.delete(article)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     with app.app_context():
