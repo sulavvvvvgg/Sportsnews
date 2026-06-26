75925824c9c1627d77efa6335fbe6a5f083a2864
Sulav Gurung <sulavgrg09@email.com>
2026-06-26 12:04:27 +0545

    Added login register and mysql database connection

--- app.py
+++ app.py
@@ -1,18 +1,67 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models import db, Article
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from models import db, Article, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sportsuser:sulavgg2005@localhost/sportsnews'
+app.config['SECRET_KEY'] = 'change-this-later'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            return "Invalid username or password"
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/')
 def home():
     articles = Article.query.all()
     return render_template('index.html', articles=articles)
 
+
 @app.route('/create', methods=['GET', 'POST'])
+@login_required
 def create_article():
     if request.method == 'POST':
         title = request.form['title']
@@ -29,6 +78,7 @@ def create_article():
     return render_template('create_article.html')
 
 @app.route('/edit/<int:article_id>', methods=['GET', 'POST'])
+@login_required
 def edit_article(article_id):
     article = Article.query.get(article_id)
 
@@ -44,6 +94,7 @@ def edit_article(article_id):
     return render_template('edit_article.html', article=article)
 
 @app.route('/delete/<int:article_id>')
+@login_required
 def delete_article(article_id):
     article = Article.query.get(article_id)
     db.session.delete(article)
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from falsk_login import UserMixin
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
@@ -14,11 +14,12 @@ class Article(db.Model):
     def __repr__(self):
         return f"<Article {self.id} - {self.title}>"
 
+
 class User(db.Model, UserMixin):
-    id = db.coloumn(db.Integer , primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
-     def __repr__(self):
+    def __repr__(self):
         return f"<User {self.id} - {self.username}>"
