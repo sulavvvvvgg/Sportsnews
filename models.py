19f378242b4d01c55ee8151f095fbc00b7b13e50
Sulav Gurung <sulavgrg09@email.com>
2026-07-03 11:34:08 +0545

    increased article title column size to 300 characters

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150), nullable=False)
+    title = db.Column(db.String(400), nullable=False)
     content = db.Column(db.Text, nullable=False)
     category = db.Column(db.String(50), nullable=False)
     author = db.Column(db.String(100), nullable=False)
