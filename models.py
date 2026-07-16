9c742f0eb758f0c290991b94d7bab6c926cb0c32
Sulav Gurung <sulavgrg09@email.com>
2026-07-16 12:10:13 +0545

    added comments crud feature with post edit and delete

--- models.py
+++ models.py
@@ -15,6 +15,16 @@ class Article(db.Model):
     def __repr__(self):
         return f"<Article {self.id} - {self.title}>"
 
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    article_id = db.Column(db.Integer, db.ForeignKey('article.id'), nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f"<Comment {self.id}>"
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
